3972335dfb8957083fa09d98f8335d557965598a
Pavel Bocharov <skidysch@mail.ru>
2023-03-06 14:27:56 +0400

    added Models, Views, and made connections between them via urls.py

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Place, Category
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'time_create',
+                    'get_html_photo', 'is_published')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create', 'categories')
+
+    fields = ('name', 'slug', 'categories', 'photo',
+              'get_html_photo', 'address', 'description',
+              'time_create', 'time_update', 'is_published')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f'<img src="{object.photo.url}" width=50')
+
+    get_html_photo.short_description = 'Miniature'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ places/mixins.py
@@ -0,0 +1,25 @@
+from django.core.cache import cache
+
+from .forms import ContactForm
+from .models import Category
+
+
+class ContextMixin:
+    shut_up = True
+
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        categories = cache.get('categories')
+        if not categories:
+            categories = Category.objects.all()
+            cache.set('categories', categories, 60)
+        form = cache.get('form')
+        if not form:
+            form = ContactForm()
+            cache.set('form', form, 60)
+        
+        context['categories'] = categories
+        context['form'] = form
+
+        return context
+        
